ad86451d53a1d23ab9b93edfaf25a1ca134befcb
krishn-kk <krishn877@gmail.com>
2021-04-01 18:06:43 +0530

    Feature : CustomAvatar added

---
+++ src/components/CustomAvatar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {Avatar } from '@material-ui/core';
+
+
+const CustomAvatar = props => {
+    return (
+        <div>
+            <Avatar
+               sizes={props.sizes}
+            />
+        </div>
+    )
+}
+
+CustomAvatar.propTypes = {
+    sizes:PropTypes.string,
+}
+CustomAvatar.defaultProps={
+    sizes:'10rem'
+}
+export default CustomAvatar
